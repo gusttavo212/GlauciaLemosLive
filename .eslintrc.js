242033b862483a1e13f91cee92467c75270385c0
gusttavo212 <gusttavo212@gmail.com>
2019-02-12 15:05:02 -0200

    ESlint Config with airbnb-base

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "plugins": [
+      "import"
+    ],
+    "rules": {
+      "no-console": "off",
+      "no unused-vars": "off",
+      "max-len": 0
+    }
+
 };
